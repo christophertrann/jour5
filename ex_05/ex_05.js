ff9c79a3dc2dcb4827807924bab5ae0780e52a20
christophertrann <christophertran@outlook.fr>
2023-10-05 13:09:03 +0200

    ex_05

---
+++ ex_05/ex_05.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    
+    var addToCartButton = document.querySelector(".cart-btn");
+  
+    addToCartButton.addEventListener("click", function () {
+     
+    alert("Thanks!");
+    });
+  });
+  
