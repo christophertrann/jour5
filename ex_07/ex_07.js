921190e7ab4c81adc5440460b295644fe0686961
christophertrann <christophertran@outlook.fr>
2023-10-05 13:20:34 +0200

    ex_07

---
+++ ex_07/ex_07.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var addToCartButton = document.querySelector(".cart-btn");
+  
+    addToCartButton.textContent = "CHANGE COLOR"; 
+  
+    addToCartButton.addEventListener("click", function () {
+      var randomColor = getRandomColor();
+      document.body.style.backgroundColor = randomColor;
+    });
+  
+    function getRandomColor() {
+      var letters = "0123456789ABCDEF";
+      var color = "#";
+      for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    }
+  });
+  
