384447aafc899719cf045d94014055fe495820d2
christophertrann <christophertran@outlook.fr>
2023-10-05 17:53:50 +0200

    ex 08

---
+++ ex_08/ex_08.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
+   
+    let descriptionParagraphs = document.querySelectorAll('#description p');
+    descriptionParagraphs[0].textContent = "never miss that perfect shot again.";
+})  
+document.addEventListener("DOMContentLoaded", function () {
+    let productNameElement = document.getElementById("productName");
+    productNameElement.textContent = "Samsung 22";
+})
+document.addEventListener("DOMContentLoaded", function () {
+    let descriptionParagraphs = document.querySelectorAll('#description p');
+    descriptionParagraphs[1].textContent = "designed to revolutionise video and photography, with beyond cinematic 16K resolution. ";
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+    let descriptionParagraphs = document.querySelectorAll('#description p');
+    descriptionParagraphs[3].textContent = "Get yours now!";
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+    let exclusiveLabel = document.getElementById("tag");
+     exclusiveLabel.remove();
+    });
+
+document.addEventListener("DOMContentLoaded", function () {
+    let priceElement = document.getElementById("price");
+    priceElement.textContent = "890 €";
+}); 
+document.addEventListener("DOMContentLoaded", function () {
+    
+ document.querySelector('img').src="Samsung22.png";
+    
+})
+document.addEventListener("DOMContentLoaded", function () {
+    let paragraph = document.getElementById('p');
+    let text = document.createTextNode('Add card : 0');
+    paragraph.appendChild(text);
+})
